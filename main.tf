755399116a7f3677a205e4968e2ebbc497b5abe6
Soham Gandhe <100147587+MrGandhe@users.noreply.github.com>
2023-02-02 13:11:18 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,29 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "stage_vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  enable_dns_hostnames = true
+}
+
+module "lambda_with_vpc" {
+  source = "./module"
+  lambda_bucket = "tes9000"
+  lambda_file_handler = "lambda_handler"
+  lambda_file_name = "${path.module}/module/python/hello-python.py.zip"
+  lambda_function_name = "test-lambda"
+  lambda_memory_size = 512
+  lambda_runtime = "python3.8"
+  prv_subnet_ids = module.vpc.private_subnets
+  vpc_lambda = module.vpc.vpc_id
+  pub_subnet_ids = module.vpc.public_subnets
+  load_balancer_name = "new-lambda"
+  target_group_name = "lambda-trg"
+}
